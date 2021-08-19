bee36c23f435e1f52304b21581a3ceb81427d15b
Lahiru Lakshan <balahirulakshan@gmail.com>
2021-08-19 22:37:02 +0530

    updated appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:device_preview/device_preview.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(
+      DevicePreview(
+        // enabled: !kReleaseMode,
+        builder: (context) => MyApp(), // Wrap your app
+      ),
+    );
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello Lahiru"),
+      locale: DevicePreview.locale(context), // Add the locale here
+      builder: DevicePreview.appBuilder, // Add the builder here
+      title: "Flutter Demo",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter"),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {  },
         ),
-        body: Center(
-          child: Text(
-            "Welcome to Flutter!",
-            style: TextStyle(fontSize: 20, color: HexColor("#8321F3"), backgroundColor: HexColor("#f39f21")),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {  },
           ),
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: () {  },
+          ),
+        ],
+        flexibleSpace: Icon(
+          Icons.photo_camera,
+          size: 75.0,
+          color: Colors.white70,
         ),
       ),
     );
