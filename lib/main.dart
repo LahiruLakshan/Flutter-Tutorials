b9975a593dc1e830567cd0005d7811f1b1ffaf36
Lahiru Lakshan <balahirulakshan@gmail.com>
2021-08-19 20:08:34 +0530

    using scaffold and hex color dependence

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
-main(){
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("Hello World"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello Lahiru"),
+        ),
+        body: Center(
+          child: Text(
+            "Welcome to Flutter!",
+            style: TextStyle(fontSize: 20, color: HexColor("#8321F3"), backgroundColor: HexColor("#f39f21")),
+          ),
+        ),
+      ),
     );
   }
 }
