e130f1f2f79c7c8f8d88a10225f2e20d89cef3bc
Lahiru Lakshan <balahirulakshan@gmail.com>
2021-08-20 19:31:00 +0530

    using Row & Column

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:hexcolor/hexcolor.dart';
 import 'package:device_preview/device_preview.dart';
 
 void main() => runApp(
@@ -39,6 +38,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 10,
         title: Text("Flutter"),
         leading: IconButton(
           icon: Icon(Icons.menu),
@@ -60,16 +60,64 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       body: Container(
-        margin: EdgeInsets.all(20),
-        height: double.infinity,
         width: double.infinity,
-        color: Colors.pink,
-        alignment: Alignment.bottomCenter,
-        child: Text(
-          "Flutter Tutorial",
-          style: TextStyle(
-            fontSize: 20.0
-          ),
+        height: double.infinity,
+        color: Colors.deepPurpleAccent,
+        // child: Column(
+        //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        //   crossAxisAlignment: CrossAxisAlignment.center,
+        //   children: [
+        //     Text(
+        //       "Text 01",
+        //       style: TextStyle(fontSize: 30, color: Colors.blue),
+        //     ),
+        //     Text(
+        //       "Text 02",
+        //       style: TextStyle(fontSize: 30, color: Colors.red),
+        //     ),
+        //     Text(
+        //       "Text 03",
+        //       style: TextStyle(fontSize: 30, color: Colors.green),
+        //     ),
+        //   ],
+        // ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Text 01",
+              style: TextStyle(fontSize: 20, color: Colors.blue, backgroundColor: Colors.white),
+            ),
+            Text(
+              "Text 02",
+              style: TextStyle(fontSize: 20, color: Colors.red, backgroundColor: Colors.white),
+            ),
+            Text(
+              "Text 03",
+              style: TextStyle(fontSize: 20, color: Colors.green, backgroundColor: Colors.white),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "Text 04",
+                  style: TextStyle(fontSize: 20, color: Colors.yellow, backgroundColor: Color.fromRGBO(
+                      255, 0, 169, 1.0)),
+                ),
+                Text(
+                  "Text 05",
+                  style: TextStyle(fontSize: 20, color: Colors.purple, backgroundColor: Color.fromRGBO(
+                      87, 255, 0, 1.0)),
+                ),
+                Text(
+                  "Text 06",
+                  style: TextStyle(fontSize: 20, color: Colors.deepOrange, backgroundColor: Colors.white),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
