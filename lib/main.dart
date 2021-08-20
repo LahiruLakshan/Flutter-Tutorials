546fc9e9262e7b784f984878344869b56af5881e
Lahiru Lakshan <balahirulakshan@gmail.com>
2021-08-20 20:42:17 +0530

    create stateful

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,91 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Flutter Tutorial",
+      home: HomePage(),
     );
   }
 }
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int count = 0;
+
+  void increment() {
+    setState(() {
+      count = count + 1;
+      print(count);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Tutorial"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "You have pushed button times",
+              style: TextStyle(fontSize: 25.0),
+            ),
+            Text(
+              '$count',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: increment,
+      ),
+    );
+  }
+}
+
+// class HomePage extends StatelessWidget {
+//   int count = 0;
+//
+//   void increment(){
+//     count = count + 1;
+//     print(count);
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Flutter Tutorial"),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Text(
+//               "You have pushed button times",
+//               style: TextStyle(fontSize: 25.0),
+//             ),
+//             Text(
+//               '$count',
+//               style: Theme.of(context).textTheme.display1,
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         child: Icon(Icons.add),
+//         onPressed: increment,
+//       ),
+//     );
+//   }
+// }
