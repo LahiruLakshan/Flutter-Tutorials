7b594e18057eee1a3a5bf36b64daa2c66d3bd531
Lahiru Lakshan <balahirulakshan@gmail.com>
2021-08-21 16:40:38 +0530

    Learning Text Field

--- lib/main.dart
+++ lib/main.dart
@@ -34,26 +34,55 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController title = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 10,
-        title: Text("Flutter"),
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: () {},
+        toolbarHeight: 80,
+        title: Padding(
+          padding: const EdgeInsets.all(1),
+          child: TextField(
+            cursorColor: Colors.black,
+            decoration: InputDecoration(
+              prefixIcon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              suffixIcon: IconButton(
+                icon: Icon(
+                  Icons.close,
+                  color: Colors.white,
+                ),
+                onPressed: (){
+                  title.text = "";
+                },
+              ),
+              filled: true,
+              hintText: "Search...",
+              hintStyle: TextStyle(color: Colors.white70),
+              border: InputBorder.none,
+            ),
+            // onChanged: (text){
+            //   print(text);
+            // },
+            // onSubmitted: (text){
+            //   print(text);
+            // },
+            controller: title,
+            keyboardType: TextInputType.text,
+            style: TextStyle(fontSize: 22),
+          ),
         ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(Icons.more_vert),
-            onPressed: () {},
-          ),
-        ],
+        // bottom: PreferredSize(
+        //     child: Container(
+        //       color: Colors.orange,
+        //       height: 4.0,
+        //     ),
+        //     preferredSize: Size.fromHeight(20.0),
+        // ),
         flexibleSpace: Image.asset(
           "assets/codex.png",
           fit: BoxFit.cover,
@@ -63,59 +92,35 @@ class _MyHomePageState extends State<MyHomePage> {
         width: double.infinity,
         height: double.infinity,
         color: Colors.deepPurpleAccent,
-        // child: Column(
-        //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        //   crossAxisAlignment: CrossAxisAlignment.center,
-        //   children: [
-        //     Text(
-        //       "Text 01",
-        //       style: TextStyle(fontSize: 30, color: Colors.blue),
-        //     ),
-        //     Text(
-        //       "Text 02",
-        //       style: TextStyle(fontSize: 30, color: Colors.red),
-        //     ),
-        //     Text(
-        //       "Text 03",
-        //       style: TextStyle(fontSize: 30, color: Colors.green),
-        //     ),
-        //   ],
-        // ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              "Text 01",
-              style: TextStyle(fontSize: 20, color: Colors.blue, backgroundColor: Colors.white),
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: TextField(
+                cursorColor: Colors.black,
+                decoration: InputDecoration(prefixIcon: Icon(Icons.search)),
+                maxLength: 8,
+                // onChanged: (text){
+                //   print(text);
+                // },
+                // onSubmitted: (text){
+                //   print(text);
+                // },
+                controller: title,
+                keyboardType: TextInputType.number,
+                style: TextStyle(fontSize: 22),
+              ),
             ),
-            Text(
-              "Text 02",
-              style: TextStyle(fontSize: 20, color: Colors.red, backgroundColor: Colors.white),
-            ),
-            Text(
-              "Text 03",
-              style: TextStyle(fontSize: 20, color: Colors.green, backgroundColor: Colors.white),
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  "Text 04",
-                  style: TextStyle(fontSize: 20, color: Colors.yellow, backgroundColor: Color.fromRGBO(
-                      255, 0, 169, 1.0)),
-                ),
-                Text(
-                  "Text 05",
-                  style: TextStyle(fontSize: 20, color: Colors.purple, backgroundColor: Color.fromRGBO(
-                      87, 255, 0, 1.0)),
-                ),
-                Text(
-                  "Text 06",
-                  style: TextStyle(fontSize: 20, color: Colors.deepOrange, backgroundColor: Colors.white),
-                ),
-              ],
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: TextField(
+                maxLength: 8,
+                autofocus: true,
+                keyboardType: TextInputType.number,
+                style: TextStyle(fontSize: 22),
+              ),
             ),
           ],
         ),
